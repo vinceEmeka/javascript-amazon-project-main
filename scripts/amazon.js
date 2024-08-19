8b499024a1aa341736a7a5a2fbc4df8e0c514e49
Vinz <abc@xyz.com>
2024-08-19 13:23:20 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,8 +79,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
 
-    console.log(cart);
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
 
 
   })
